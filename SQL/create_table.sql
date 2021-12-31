c3c140dda01384d2452af5a2aaf8f47d847105c3
Nicholas <NicholasBoeke@gmail.com>
2021-12-30 19:02:36 -0600

    Update create_table.sql

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -1,9 +1,8 @@
 
 CREATE TABLE test_table ( A VARCHAR2(26),  B NUMBER(38));
-create table test_table (A varchar(26), B number(38) );
+
 INSERT INTO test_table (A, B) VALUES ('a',1);
 INSERT INTO test_table (A, B) VALUES ('b',2);
 INSERT INTO test_table (A, B) VALUES ('c',3);
 
 drop table test_table;
-
