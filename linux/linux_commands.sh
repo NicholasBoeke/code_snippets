8fb25b05c3366f2cd1262885e54616e49c732721
Nicholas <NicholasBoeke@gmail.com>
2023-04-26 08:21:42 -0500

    Update linux_commands.sh

--- linux/linux_commands.sh
+++ linux/linux_commands.sh
@@ -11,5 +11,9 @@ which python3
 
 # memory usage on linux
 cat /proc/meminfo
-free
-vmstat
+  file holding info on memory stats
+  
+free - Display amount of free and used memory in the system. displays the total amount of free and used physical and swap memory in the system, as well as the buffers and caches used by the ker‐
+       nel. The information is gathered by parsing /proc/meminfo.
+
+vmstat - Report virtual memory statistics. reports information about processes, memory, paging, block IO, traps, disks and cpu activity.
